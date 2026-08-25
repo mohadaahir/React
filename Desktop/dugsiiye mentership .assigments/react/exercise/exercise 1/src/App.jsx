8cb24994454ff443791051988c57135e1d5ad4a3
mohadaahir <mohadaahir2023@gmail.com>
2026-08-25 21:39:48 +0300

    exercise

---
+++ Desktop/dugsiiye mentership .assigments/react/exercise/exercise 1/src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+import {Usercard} from './custom'
+
+function App() {
+  return (
+    <p><Usercard/> </p>
+  )
+}
+
+export default App
---
+++ Desktop/dugsiiye mentership .assigments/react/exercise/exercise 1/src/custom.jsx
@@ -0,0 +1,8 @@
+export function Usercard(){
+    const userName="mohamed "
+    const email="moha@gmail.com"
+return(<div>
+    <h2>my name is {userName}</h2>
+    <span >my email is {email}</span >
+</div>)
+}
