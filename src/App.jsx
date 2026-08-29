c63854c833b607c9eaf37806745dac190fa448ab
mohadaahir <mohadaahir2023@gmail.com>
2026-08-29 20:10:16 +0300

    Initial React project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+import UserCard from './custom'
+function App() {
+
+  return (
+  <UserCard/>
+  
+  )
+}
+
+export default App
---
+++ src/Custom.jsx
@@ -0,0 +1,9 @@
+function UserCard (){
+    return (
+        <div>
+            <h1>mohamed</h1>
+            <p>moha@gmail.com</p>
+        </div>
+    )
+}
+export default UserCard
